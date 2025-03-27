54bd5e37994a02a556ae94f3258cc4bfa1f0fb67
secre-alt <triannesano@gmail.com>
2025-03-26 23:25:06 -0700

    up

--- myproject/post/models.py
+++ myproject/post/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
+    Body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    
